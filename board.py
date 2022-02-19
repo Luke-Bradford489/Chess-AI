f3aeecbba2d7eadb3e05e8703979aa7e02fe9df9
L_Bradford <u18185208@tuks.co.za>
2022-02-19 08:17:44 +0200

    Small changes

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import numpy as np
 from enum import Enum
+import pieces
 
 class Piece(Enum):
       EMPTY = 0
@@ -24,45 +25,70 @@ class ChessBoard:
             for i in cls.board:
                   lineOutput = ""
                   for j in i:
-                        lineOutput += (" | " + str(j.value) + " | ")
+                        lineOutput += (" | " + j.name + " | ")
                   lineOutput += "\n"
                   for j in i:
-                        lineOutput += ("   _   ")
+                        lineOutput += ("   -   ")
                   print(lineOutput)
+
+      @classmethod
+      def isPieceAt(cls,row,col):
+            if cls.board[row][col] == 0:
+                  return False
+            else:
+                  return True
+
+      @classmethod
+      def getPiece(cls,row,col):
+            if cls.isPieceAt(row,col):
+                  if cls.board[row][col] == 1 :
+                        return "Pawn"
+                  elif cls.board[row][col] == 2:
+                        return "Rook"
+                  elif cls.board[row][col] == 3:
+                        return "Bishop"
+                  elif cls.board[row][col] == 4:
+                        return "Knight"
+                  elif cls.board[row][col] == 5:
+                        return "Queen"
+                  elif cls.board[row][col] == 6:
+                        return "King"
+                  else:
+                        return "Black Magic Fuckery"
+
 
       @classmethod
       def initializeBoard(cls):
             
             for i in range(0,8):
-                  cls.board[1][i] = Piece.PAWN
-                  cls.board[6][i] = Piece.PAWN
+                  cls.board[1][i] = pieces.Pawn
+                  cls.board[6][i] = pieces.Pawn
                   
             
-            cls.board[0][0] = Piece.ROOK
-            cls.board[0][1] = Piece.KNIGHT
-            cls.board[0][2] = Piece.BISHOP
-            cls.board[0][3] = Piece.QUEEN
-            cls.board[0][4] = Piece.KING
-            cls.board[0][5] = Piece.BISHOP
-            cls.board[0][6] = Piece.KNIGHT
-            cls.board[0][7] = Piece.ROOK
+            cls.board[0][0] = pieces.Rook
+            cls.board[0][1] = pieces.Knight
+            cls.board[0][2] = pieces.Bishop
+            cls.board[0][3] = pieces.Queen
+            cls.board[0][4] = pieces.King
+            cls.board[0][5] = pieces.Bishop
+            cls.board[0][6] = pieces.Knight
+            cls.board[0][7] = pieces.Rook
 
-            cls.board[7][0] = Piece.ROOK
-            cls.board[7][1] = Piece.KNIGHT
-            cls.board[7][2] = Piece.BISHOP
-            cls.board[7][3] = Piece.QUEEN
-            cls.board[7][4] = Piece.KING
-            cls.board[7][5] = Piece.BISHOP
-            cls.board[7][6] = Piece.KNIGHT
-            cls.board[7][7] = Piece.ROOK
+            cls.board[7][0] = pieces.Rook
+            cls.board[7][1] = pieces.Knight
+            cls.board[7][2] = pieces.Bishop
+            cls.board[7][3] = pieces.Queen
+            cls.board[7][4] = pieces.King
+            cls.board[7][5] = pieces.Bishop
+            cls.board[7][6] = pieces.Knight
+            cls.board[7][7] = pieces.Rook
 
 
             return cls
 
             
 
-
-            
+print(pieces.Rook.name)
             
 
 test1 = ChessBoard(ID="Test1")
--- board_controller.py
+++ board_controller.py
@@ -1,4 +1,13 @@
-
+from mimetypes import init
+import board
+import pieces
 
 class BoardController:
-      pass
+      ID = ""
+
+      def __init__(self,ID):
+            self.ID = ID
+            self.chessBoard = board.ChessBoard(ID)
+
+      def AttemptMove():
+            pass
