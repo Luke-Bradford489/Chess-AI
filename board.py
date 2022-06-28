5e5ae40d740e4ef6ac7250361067c23310be4aa2
L_Bradford <u18185208@tuks.co.za>
2022-06-28 11:51:49 +0200

    Began logic implementation for board controller

--- board.py
+++ board.py
@@ -1,100 +1,80 @@
 import numpy as np
 import pieces
 from enum import Enum
-import pieces
 
-class Piece(Enum):
+class ePiece(Enum):
       EMPTY = 0
-      pieces.Pawn = 1
-      pieces.Rook = 2
-      pieces.Bishop = 3
-      pieces.Knight = 4
-      pieces.Queen = 5
-      pieces.King = 6
+      PAWN = 1
+      ROOK = 2
+      BISHOP = 3
+      KNIGHT = 4
+      QUEEN = 5
+      KING = 6
 
 
 class ChessBoard:
-      
-      board = np.full((8,8), Piece.EMPTY)
+
       def __init__(self, ID):
           self.ID = ID
+          self.board = np.full((8, 8), 0)
           
-          
-          
-      @classmethod
-      def display(cls):
-            for i in cls.board:
+
+      def display(self):
+            for i in self.board:
                   lineOutput = ""
                   for j in i:
-                        lineOutput += (" | " + j.name + " | ")
+                       if j == 0:
+                            lineOutput += (" | " + "EMPTY" + " | ")
+                       else:
+                            lineOutput += (" | " + j.name + " | ")
                   lineOutput += "\n"
                   for j in i:
                         lineOutput += ("   -   ")
                   print(lineOutput)
 
-      @classmethod
-      def isPieceAt(cls,row,col):
-            if cls.board[row][col] == 0:
+
+      def isPieceAt(self,row,col):
+            if self.board[row][col] == 0:
                   return False
             else:
                   return True
 
-      @classmethod
-      def getPiece(cls,row,col):
-            if cls.isPieceAt(row,col):
-                  if cls.board[row][col] == 1 :
-                        return "Pawn"
-                  elif cls.board[row][col] == 2:
-                        return "Rook"
-                  elif cls.board[row][col] == 3:
-                        return "Bishop"
-                  elif cls.board[row][col] == 4:
-                        return "Knight"
-                  elif cls.board[row][col] == 5:
-                        return "Queen"
-                  elif cls.board[row][col] == 6:
-                        return "King"
-                  else:
-                        return "Black Magic Fuckery"
+
+      def getPiece(self,row,col):
+            if self.isPieceAt(row,col):
+                  return self.board[row][col]
 
 
-      @classmethod
-      def initializeBoard(cls):
-            
+      def initializeBoard(self):
+
             for i in range(0,8):
-                  cls.board[1][i] = pieces.Pawn('b',1,i)
-                  cls.board[6][i] = pieces.Pawn('w',1,i)
-
-                  
-            
-            cls.board[0][0] = pieces.Rook
-            cls.board[0][1] = pieces.Knight
-            cls.board[0][2] = pieces.Bishop
-            cls.board[0][3] = pieces.Queen
-            cls.board[0][4] = pieces.King
-            cls.board[0][5] = pieces.Bishop
-            cls.board[0][6] = pieces.Knight
-            cls.board[0][7] = pieces.Rook
-
-            cls.board[7][0] = pieces.Rook
-            cls.board[7][1] = pieces.Knight
-            cls.board[7][2] = pieces.Bishop
-            cls.board[7][3] = pieces.Queen
-            cls.board[7][4] = pieces.King
-            cls.board[7][5] = pieces.Bishop
-            cls.board[7][6] = pieces.Knight
-            cls.board[7][7] = pieces.Rook
+                  self.board[1][i] = pieces.Pawn('b',1 ,i)
+                  self.board[6][i] = pieces.Pawn('w',1 ,i)
 
 
-            return cls
 
-            
+            self.board[0][0] = pieces.Rook('b',0 ,0)
+            self.board[0][1] = pieces.Knight('b',0 ,1)
+            self.board[0][2] = pieces.Bishop('b',0 ,2)
+            self.board[0][3] = pieces.Queen('b',0 ,3)
+            self.board[0][4] = pieces.King('b',0 ,4)
+            self.board[0][5] = pieces.Bishop('b',0 ,5)
+            self.board[0][6] = pieces.Knight('b',0 ,6)
+            self.board[0][7] = pieces.Rook('b',0 ,7)
 
-print(pieces.Rook.name)
-            
+            self.board[7][0] = pieces.Rook('w',7 ,0)
+            self.board[7][1] = pieces.Knight('w',7 ,1)
+            self.board[7][2] = pieces.Bishop('w',7 ,2)
+            self.board[7][3] = pieces.Queen('w',7 ,3)
+            self.board[7][4] = pieces.King('w',7 ,4)
+            self.board[7][5] = pieces.Bishop('w',7 ,5)
+            self.board[7][6] = pieces.Knight('w',7 ,6)
+            self.board[7][7] = pieces.Rook('w',7 ,7)
+
+
 
 test1 = ChessBoard(ID="Test1")
-test1 = test1.initializeBoard()
+test1.initializeBoard()
 test1.display()
 
 
--- board_controller.py
+++ board_controller.py
@@ -2,12 +2,24 @@ from mimetypes import init
 import board
 import pieces
 
+
 class BoardController:
-      ID = ""
 
-      def __init__(self,ID):
-            self.ID = ID
-            self.chessBoard = board.ChessBoard(ID)
+     def __init__(self, ID):
+          self.ID = ID
+          self.chessBoard = board.ChessBoard(ID)
 
-      def AttemptMove():
-            pass
+     def AttemptMove(self, startRow, startCol, endRow, endCol):
+          if self.chessBoard.isPieceAt(startCol, startRow):
+               piece = self.chessBoard.getPiece(startRow, startCol)
+               if piece.legalMove(startRow, startCol, endRow, endCol):
+                    piece.move(startRow, startCol, endRow, endCol)
+
+                    return True
+          else:
+               return False
+
+
+BoardController = BoardController("123")
+print(BoardController.AttemptMove(1,1,2,1))
+
